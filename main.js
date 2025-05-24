a169de90791ddb489c2a3f69ee5a5fa11434f553
ciberOn <classkiberone@gmail.com>
2025-05-24 16:09:51 +0300

    Hi Iam from the future

---
+++ main.js
@@ -0,0 +1,20 @@
+const arrVariants = [
+    "yes",
+    "no",
+    "maybe",
+    "chocolate",
+    "if you try hartd",
+    "i can't say directly",
+    "Ask another one",
+    "i'm not ready",
+    "i think another time",
+    "it's will be easy",
+    "i can not say now",
+    "Байден",
+]
+
+const btn = document.getElementById("btn")
+btn.addEventListener("click", () => {
+    const randInt = Math.floor(Math.random() * arrVariants.length)
+    alert(arrVariants[randInt])
+})
